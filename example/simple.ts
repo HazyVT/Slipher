aadaf59af01e98febf88a8207275f11c7a17a45c
Mohammed Salim <renablehots@gmail.com>
2024-03-03 15:34:17 +0400

    Remove unneeded files

---
+++ example/simple.ts
@@ -0,0 +1,29 @@
+import { Wave } from "../index";
+
+function load() {
+    screen.setSize(1280, 720);
+}
+
+function update() {
+    const event = Wave.event.get();
+
+    Wave.event.handleEvent(event);
+}
+
+function draw() {
+    Wave.graphics.setColor(255,255,255,1);
+    Wave.graphics.rectangle('fill', 0, 0, screen.getWidth(), screen.getHeight());
+
+    Wave.graphics.flip();
+}
+
+Wave.init();
+const screen = Wave.createWindow(640, 480);
+load();
+
+while(Wave.running) {
+    update();
+    draw();
+}
+
+Wave.quit();
